ddefd7489b81f85f5a1b5ccb44e4d76e6e192bf0
iDataist <hwdgrmy@gmail.com>
2019-11-28 15:04:28 -0600

    Update train.py

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -5,9 +5,7 @@ import os
 import pandas as pd
 
 from sklearn.externals import joblib
-
-## TODO: Import any additional libraries you need to define a model
-
+from sklearn.linear_model import LogisticRegression
 
 # Provided model load function
 def model_fn(model_dir):
@@ -15,31 +13,22 @@ def model_fn(model_dir):
     in the main if statement.
     """
     print("Loading model.")
-    
+
     # load using joblib
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     print("Done loading model.")
-    
+
     return model
 
-
-## TODO: Complete the main code
 if __name__ == '__main__':
-    
-    # All of the model parameters and training parameters are sent as arguments
-    # when this script is executed, during a training job
-    
-    # Here we set up an argument parser to easily access the parameters
+
     parser = argparse.ArgumentParser()
 
     # SageMaker parameters, like the directories for training data and saving models; set automatically
-    # Do not need to change
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
-    ## TODO: Add any additional arguments that you will need to pass into your model
-    
+
     # args holds all passed-in arguments
     args = parser.parse_args()
 
@@ -50,21 +39,9 @@ if __name__ == '__main__':
     # Labels are in the first column
     train_y = train_data.iloc[:,0]
     train_x = train_data.iloc[:,1:]
-    
-    
-    ## --- Your code here --- ##
-    
 
-    ## TODO: Define a model 
-    model = None
-    
-    
-    ## TODO: Train the model
-    
-    
-    
-    ## --- End of your code  --- ##
-    
+    model = LogisticRegression(solver = 'liblinear')
+    model.fit(train_x, train_y)
 
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
